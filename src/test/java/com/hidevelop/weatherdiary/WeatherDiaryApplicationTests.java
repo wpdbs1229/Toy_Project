3f329c27d262840ec17838879776292edc078651
JaYoun-Lee <dlwpdbs1229@naver.com>
2023-03-15 12:18:51 +0900

    프로젝트 설정 -Lombok -Spring Web -jdk 1.8 -jar

---
+++ src/test/java/com/hidevelop/weatherdiary/WeatherDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidevelop.weatherdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
