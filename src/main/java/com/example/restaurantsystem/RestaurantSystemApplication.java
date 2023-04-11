630f584a7d6bddfd22c46140a95980c709c250cd
JaYoun-Lee <dlwpdbs1229@naver.com>
2023-04-11 16:45:05 +0900

    Initial commit

---
+++ src/main/java/com/example/restaurantsystem/RestaurantSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantSystemApplication.class, args);
+    }
+
+}
